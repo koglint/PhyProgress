950bba7657959cfb0f80d7c8f64ad844c5fe8d15
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2024-10-10 14:49:50 +1100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,28 @@
+// Import the functions you need from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD346cKjY_UadNJqGW2Ndmk3LZ2RVr2HIU",
+  authDomain: "koglinsphysicsclass.firebaseapp.com",
+  projectId: "koglinsphysicsclass",
+  storageBucket: "koglinsphysicsclass.appspot.com",
+  messagingSenderId: "1051481871959",
+  appId: "1:1051481871959:web:40bc24648d3a4f91f87512"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+// Register function
+export function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Login function
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
 
