85a0acf6aee19ed30c53142c71f42536582642d8
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2024-10-12 13:40:20 +1100

    Update lessonLinks.js new links

--- lessonLinks.js
+++ lessonLinks.js
@@ -33,9 +33,22 @@ export const lessonNames = {
 };
 
 export const lessonLinks = {
-  '5.1-1': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/hsc-classes/module-5-lessons/module-5-1#h.77yezm3v091b',
-  '5.1-2': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/hsc-classes/module-5-lessons/module-5-1#h.w9y76slkc7tp',
-  '5.1-3': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/hsc-classes/module-5-lessons/module-5-1#h.7ktddjo0ng7c',
+  '5.1-1': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-1#h.77yezm3v091b',
+  '5.1-2': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-1#h.w9y76slkc7tp',
+  '5.1-3': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-1#h.7ktddjo0ng7c',
+  '5.2-1': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.jjdrsdnvcz4f',
+  '5.2-2': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.lfrhf619napn',
+  '5.2-3': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.kd3c8kt9hppf',
+  '5.2-4': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.yxgx9hk0r0gl',
+  '5.2-5': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.38slq1k07dlp',
+  '5.2-6': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-2#h.g7dfb1mf9m7u',
+  '5.3-1': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.7fkhrgs5ao06',
+  '5.3-2': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.f4mj17826ftk',
+  '5.3-3': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.f74ccta4gy4i',
+  '5.3-4': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.acffqmnkbull',
+  '5.3-5': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.inkh5ed8p88n',
+  '5.3-6': 'https://sites.google.com/education.nsw.gov.au/koglinphysics/lessons/module-5-3#h.skem8i1q43xe',
+  
   // Add more links as needed
 };
 
